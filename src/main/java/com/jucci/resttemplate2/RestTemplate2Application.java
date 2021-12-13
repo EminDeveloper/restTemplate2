ba4baa552a48464739e3344b52d03bf3405a3d76
Spring-stack <Spring-stack>
2021-12-13 09:12:37 -0800

    Create Project

---
+++ src/main/java/com/jucci/resttemplate2/RestTemplate2Application.java
@@ -0,0 +1,13 @@
+package com.jucci.resttemplate2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestTemplate2Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RestTemplate2Application.class, args);
+    }
+
+}
