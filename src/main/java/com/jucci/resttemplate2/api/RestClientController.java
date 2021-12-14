d40567d1e078e5ccbfd9214a76dda5efc7abbf60
Spring-stack <Spring-stack>
2021-12-13 16:57:34 -0800

    add configuration

--- src/main/java/com/jucci/resttemplate2/api/RestClientController.java
+++ src/main/java/com/jucci/resttemplate2/api/RestClientController.java
@@ -5,6 +5,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.client.RestTemplate;
 
@@ -24,6 +26,13 @@ public class RestClientController {
         ResponseEntity<List> result = restTemplate.getForEntity(webUrl, List.class);
         List<EmployeeDTO> responseBody = result.getBody();
 
+        return ResponseEntity.ok(responseBody);
+    }
+
+    @PostMapping
+    public ResponseEntity<EmployeeDTO> save(@RequestBody EmployeeDTO employeeDTO){
+        ResponseEntity<EmployeeDTO> result = restTemplate.postForEntity(webUrl, employeeDTO, EmployeeDTO.class);
+        EmployeeDTO responseBody = result.getBody();
         return ResponseEntity.ok(responseBody);
     }
 }
